2255231eca366b5e01155121f5155a55607e9c0d
Magezi Arthur <artmagezi@gmail.com>
2021-12-10 05:35:38 +0300

    default data migrations

---
+++ fineract-provider/src/main/resources/sql/migrations/core_db/V1005__velocity_credits_product.sql
@@ -0,0 +1,68 @@
+--
+-- Licensed to the Apache Software Foundation (ASF) under one
+-- or more contributor license agreements. See the NOTICE file
+-- distributed with this work for additional information
+-- regarding copyright ownership. The ASF licenses this file
+-- to you under the Apache License, Version 2.0 (the
+-- "License"); you may not use this file except in compliance
+-- with the License. You may obtain a copy of the License at
+--
+-- http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing,
+-- software d istributed under the License is distributed on an
+-- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+-- KIND, either express or implied. See the License for the
+-- specific language governing permissions and limitations
+-- under the License.
+--
+
+
+INSERT IGNORE INTO `m_savings_product` (
+  `id`, `name`, `short_name`, `description`,
+  `deposit_type_enum`, `currency_code`,
+  `currency_digits`, `currency_multiplesof`,
+  `nominal_annual_interest_rate`,
+  `interest_compounding_period_enum`,
+  `interest_posting_period_enum`,
+  `interest_calculation_type_enum`,
+  `interest_calculation_days_in_year_type_enum`,
+  `min_required_opening_balance`,
+  `lockin_period_frequency`, `lockin_period_frequency_enum`,
+  `accounting_type`, `withdrawal_fee_amount`,
+  `withdrawal_fee_type_enum`, `withdrawal_fee_for_transfer`,
+  `allow_overdraft`, `overdraft_limit`,
+  `nominal_annual_interest_rate_overdraft`,
+  `min_overdraft_for_interest_calculation`,
+  `min_required_balance`, `enforce_min_required_balance`,
+  `min_balance_for_interest_calculation`,
+  `withhold_tax`, `tax_group_id`,
+  `is_dormancy_tracking_active`,
+  `days_to_inactive`, `days_to_dormancy`,
+  `days_to_escheat`
+)
+VALUES
+(
+    1, 'Velocity Credits', 'VCR', 'This holds Credits',
+    100, 'CRD', 2, 1, 0.000000, 1, 4, 1, 365,
+    NULL, NULL, NULL, 2, NULL, NULL, 0, 0,
+    NULL, NULL, NULL, NULL, 0, NULL, 0, NULL,
+    0, NULL, NULL, NULL
+);
+
+-- adding product mappings
+INSERT IGNORE INTO `acc_product_mapping` (
+  `id`, `gl_account_id`, `product_id`,
+  `product_type`, `payment_type`,
+  `charge_id`, `financial_account_type`
+)
+VALUES
+  (10, 3, 1, 2, NULL, NULL, 1),
+  (11, 25, 1, 2, NULL, NULL, 11),
+  (12, 94, 1, 2, NULL, NULL, 4),
+  (13, 88, 1, 2, NULL, NULL, 5),
+  (14, 107, 1, 2, NULL, NULL, 12),
+  (15, 109, 1, 2, NULL, NULL, 3),
+  (16, 114, 1, 2, NULL, NULL, 13),
+  (17, 43, 1, 2, NULL, NULL, 2),
+  (18, 47, 1, 2, NULL, NULL, 10);
