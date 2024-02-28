1a2c79bb4c157eee40951dd80a9dbf9efe34ffd2
sloops77 <andres.olave@thinkingbytes.net>
2024-02-28 12:18:35 +1100

    fix: build errors before merge

--- fineract-provider/src/main/resources/sql/migrations/core_db/V1002__velocity_credit_currency.sql
+++ fineract-provider/src/main/resources/sql/migrations/core_db/V1002__velocity_credit_currency.sql
@@ -10,12 +10,13 @@
 -- http://www.apache.org/licenses/LICENSE-2.0
 --
 -- Unless required by applicable law or agreed to in writing,
--- software d istributed under the License is distributed on an
+-- software distributed under the License is distributed on an
 -- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 -- KIND, either express or implied. See the License for the
 -- specific language governing permissions and limitations
 -- under the License.
 --
+
 INSERT IGNORE INTO `m_currency` (`code`, `decimal_places`, `currency_multiplesof`, `display_symbol`, `name`, `internationalized_name_code`)
 VALUES
     ('CRD', 2, NULL, NULL, 'Velocity Credits', 'currency.CRD');
